78f25e84ed289afdb44f4db940b923ce6576951a
c9s <yoanlin93@gmail.com>
2015-08-10 19:25:14 +0800

    Add CRUDOperation

---
+++ src/Kendo/Operation/CRUDOperation.php
@@ -0,0 +1,13 @@
+<?php
+namespace Kendo\Operation;
+
+class CRUDOperation
+{
+    const CREATE = 1001;
+    const UPDATE = 1002;
+    const DELETE = 1003;
+    const VIEW   = 1004;
+    const SEARCH = 1005;
+}
+
+
