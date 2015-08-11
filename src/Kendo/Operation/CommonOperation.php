0279b412e3a9f8135eec0d4a07f68c28f5f3245d
c9s <yoanlin93@gmail.com>
2015-08-11 19:35:33 +0800

    Fix bitmask OR expression

--- src/Kendo/Operation/CommonOperation.php
+++ src/Kendo/Operation/CommonOperation.php
@@ -6,7 +6,7 @@ namespace Kendo\Operation;
  */
 class CommonOperation
 {
-    const ALL    = 1 || 1 << 2 || 1 << 3 || 1 << 4 || 1 << 5; // 11111
+    const ALL    = 1 | 1 << 2 | 1 << 3 | 1 << 4 | 1 << 5; // 11111
     const CREATE = 1;       // 00001
     const UPDATE = 1 << 2;  // 00010
     const DELETE = 1 << 3;  // 00100
