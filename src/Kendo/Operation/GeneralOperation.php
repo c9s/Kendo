bd7a677d2d099350c898238b0d61ae495ebd1ba1
c9s <yoanlin93@gmail.com>
2015-08-12 12:34:24 +0800

    remove shift-left operator from class definition

--- src/Kendo/Operation/GeneralOperation.php
+++ src/Kendo/Operation/GeneralOperation.php
@@ -10,10 +10,10 @@ class GeneralOperation implements OperationDefinitionSet
     // const ALL    = 1 | 1 << 2 | 1 << 3 | 1 << 4 | 1 << 5; // 11111
     const ALL    = 61;
     const CREATE = 1;       // 00001
-    const UPDATE = 1 << 2;  // 00010
-    const DELETE = 1 << 3;  // 00100
-    const VIEW   = 1 << 4;  // 01000
-    const SEARCH = 1 << 5;  // 10000
+    const UPDATE = 2; // 1 << 2;  // 00010
+    const DELETE = 4; // 1 << 3;  // 00100
+    const VIEW   = 8; // 1 << 4;  // 01000
+    const SEARCH = 16; // 1 << 5;  // 10000
 
     public function export()
     {
