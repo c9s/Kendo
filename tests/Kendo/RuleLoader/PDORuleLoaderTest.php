2d5df511936abe28827eeed6aee9b3eefa9d165f
c9s <yoanlin93@gmail.com>
2015-08-12 15:30:35 +0800

    Add PDORuleLoaderTest

---
+++ tests/Kendo/RuleLoader/PDORuleLoaderTest.php
@@ -0,0 +1,81 @@
+<?php
+use LazyRecord\ConnectionManager;
+use LazyRecord\Testing\ModelTestCase;
+
+use SimpleApp\SimpleSecurityPolicy;
+use SimpleApp\User\NormalUser;
+
+use Kendo\RuleLoader\RuleLoader;
+use Kendo\RuleLoader\PDORuleLoader;
+use Kendo\RuleLoader\SecurityPolicyRuleLoader;
+use Kendo\RuleMatcher\AccessRuleMatcher;
+
+use Kendo\Operation\GeneralOperation;
+use Kendo\Authorizer\Authorizer;
+use Kendo\SecurityPolicy\SecurityPolicyModule;
+
+use Kendo\RuleExporter\DatabaseRuleExporter;
+
+use Kendo\Model\AccessRuleCollection;
+use Kendo\Model\ActorCollection;
+use Kendo\Model\ResourceCollection;
+
+class PDORuleLoaderTest extends ModelTestCase
+{
+    public $driver = 'sqlite';
+
+    public function getModels()
+    {
+        return [
+            new \Kendo\Model\ActorSchema,
+            new \Kendo\Model\RoleSchema,
+            new \Kendo\Model\ResourceSchema,
+            new \Kendo\Model\OperationSchema,
+            new \Kendo\Model\AccessRuleSchema,
+            new \Kendo\Model\AccessControlSchema,
+        ];
+    }
+
+    public function testLoad()
+    {
+        $storage = new SecurityPolicyModule;
+        $storage->add(new SimpleSecurityPolicy);
+        $loader = new SecurityPolicyRuleLoader;
+        $loader->load($storage);
+
+        $exporter = new DatabaseRuleExporter($loader);
+        $exporter->export();
+
+        $resources = new ResourceCollection;
+        $this->assertCount(2, $resources);
+
+        $connectionManager = ConnectionManager::getInstance();
+        $this->assertNotNull($connectionManager);
+
+        $conn = $connectionManager->getConnection($this->getDriverType());
+        $this->assertNotNull($conn);
+
+        $loader = new PDORuleLoader();
+        $loader->load($conn);
+
+        $this->assertCount(2, $resources = $loader->getResourceDefinitions());
+        $this->assertCount(3, $actors = $loader->getActorDefinitions());
+        $this->assertCount(6, $ops = $loader->getOperationDefinitions());
+
+        /*
+        $rules = new AccessRuleCollection;
+        $this->assertCount(4, $rules);
+
+        $actors = new ActorCollection;
+        $this->assertCount(3, $actors);
+        foreach ($actors as $actor) {
+            $this->assertNotEmpty($actor);
+            $this->assertNotNull($actor->id);
+        }
+         */
+
+
+
+    }
+}
+
