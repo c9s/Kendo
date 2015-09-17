f419f332c1a86e4abf2ad3560d822085e8687eeb
c9s <yoanlin93@gmail.com>
2015-09-17 16:38:46 +0800

    Add SecondSecurityPolicy

---
+++ tests/SimpleApp/SecondSecurityPolicy.php
@@ -0,0 +1,14 @@
+<?php
+namespace SimpleApp;
+use Kendo\SecurityPolicy\RBACSecurityPolicySchema;
+use Kendo\SecurityPolicy\SecurityPolicyModule;
+use Kendo\Operation\GeneralOperation;
+
+class SecondSecurityPolicy extends RBACSecurityPolicySchema
+{
+    public function schema()
+    {
+
+
+    }
+}
