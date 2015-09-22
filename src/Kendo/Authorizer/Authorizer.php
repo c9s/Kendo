0674f539f5ec805825a9328e3481a8e1f733d6b1
c9s <yoanlin93@gmail.com>
2015-09-22 13:08:45 +0800

    Add properties to Authorizer class

--- src/Kendo/Authorizer/Authorizer.php
+++ src/Kendo/Authorizer/Authorizer.php
@@ -2,9 +2,41 @@
 namespace Kendo\Authorizer;
 use Kendo\RuleMatcher\RuleMatcher;
 
+class Credential
+{
+
+    public $expiry; // timestamp or DateTime
+
+    public $actor;
+}
+
+class Result {
+
+    public $allowed;
+
+    public $reason;
+
+    public $credential;
+
+    public function __construct($allowed, $reason, Credential $credential = null)
+    {
+        $this->allowed = $allowed;
+        $this->reason = $reason;
+        $this->credential = $credential;
+    }
+
+}
+
 class Authorizer
 {
     protected $matchers = array();
+
+    /**
+     * @var int $ttl time to live.
+     */
+    protected $ttl = 30;
+
+    protected $disallowNoRuleMatches = true;
 
     public function __construct()
     {
@@ -19,8 +51,14 @@ class Authorizer
     public function authorize($actor, $operation, $resource)
     {
         foreach ($this->matchers as $matcher) {
-            $matcher->match($actor, $operation, $resource);
+            if ($result = $matcher->match($actor, $operation, $resource)) {
+                return $result;
+            }
         }
+        if ($this->disallowNoRuleMatches) {
+            return new Result(false, "Disallowed.");
+        }
+        return new Result(true, "Allowed by default.");
     }
 }
 
