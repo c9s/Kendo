02a8a4381d7b88956d526782690ec8fb77d01616
c9s <yoanlin93@gmail.com>
2015-09-25 20:58:11 +0800

    Remove All operation since we've removed the bitmask

--- src/Kendo/Operation/GeneralOperation.php
+++ src/Kendo/Operation/GeneralOperation.php
@@ -9,13 +9,11 @@ namespace Kendo\Operation;
  */
 class GeneralOperation
 {
-    // const ALL    = 1 | 1 << 2 | 1 << 3 | 1 << 4 | 1 << 5; // 11111
-    const ALL    = 'all';
-    const CREATE = 'create';       // 00001
-    const UPDATE = 'update'; // 1 << 2;  // 00010
-    const DELETE = 'delete'; // 1 << 3;  // 00100
-    const VIEW   = 'view'; // 1 << 4;  // 01000
-    const SEARCH = 'search'; // 1 << 5;  // 10000
+    const CREATE = 'create';
+    const UPDATE = 'update';
+    const DELETE = 'delete';
+    const VIEW   = 'view';
+    const SEARCH = 'search';
 }
 
 
