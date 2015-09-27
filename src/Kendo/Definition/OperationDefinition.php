6d28c09cb3ad072d566c57212599c5edb0be3c38
c9s <yoanlin93@gmail.com>
2015-09-27 17:04:56 +0800

    Extend OperationDefinition from BaseDefinition

--- src/Kendo/Definition/OperationDefinition.php
+++ src/Kendo/Definition/OperationDefinition.php
@@ -3,33 +3,8 @@ namespace Kendo\Definition;
 use Kendo\SecurityPolicy\RBACSecurityPolicySchema;
 use Exception;
 
-class OperationDefinition
+class OperationDefinition extends BaseDefinition
 {
-    /**
-     * @var integer id is used for fetched object from PDO
-     */
-    public $id;
-
-    public $identifier;
-
-    public $label;
-
-    public $description;
-
-    public function __construct($identifier = null, $label = null)
-    {
-        if ($identifier) {
-            $this->identifier = $identifier;
-        }
-        if ($label) {
-            $this->label = $label;
-        }
-
-        // Record Id
-        if ($this->id) {
-            $this->id = intval($this->id);
-        }
-    }
 }
 
 
