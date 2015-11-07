eb3d005a17ebf7ecf3ac7800eac69a38f4902e79
c9s <yoanlin93@gmail.com>
2015-11-07 12:32:42 +0800

    Add more doc

--- src/Kendo/RuleMatcher/RuleMatcher.php
+++ src/Kendo/RuleMatcher/RuleMatcher.php
@@ -2,6 +2,11 @@
 namespace Kendo\RuleMatcher;
 use Kendo\Context;
 
+
+/**
+ * A RuleMatcher should provide a match method to match a rule array and return
+ * the matched rule array
+ */
 interface RuleMatcher 
 {
     const RESOURCE_RULE_UNDEFINED = 1;
@@ -9,6 +14,15 @@ interface RuleMatcher
     const ACTOR_RULE_UNDEFINED    = 3;
     const NO_RULE_MATCHED         = 4;
 
+
+    /**
+     *
+     * @param string|GeneralIdentifierProvider|ActorIdentifierProvider An object that implemented with
+     *                                         ActorIdentifierProvider or the actor string identifier.
+     *
+     * @param string $opeation operation identifier.
+     * @param string|ResourceIdentifierProvider|GeneralIdentifierProvider
+     */
     public function match($actor, $operation, $resource, Context $context = null);
 
 }
